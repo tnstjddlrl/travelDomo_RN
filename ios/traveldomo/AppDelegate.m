fa60ee54a1dab8438e72b2f4c2f7f99c93f86a94
tnstjddlrl <48933775+tnstjddlrl@users.noreply.github.com>
2021-12-17 15:24:51 +0900

    test

--- ios/traveldomo/AppDelegate.m
+++ ios/traveldomo/AppDelegate.m
@@ -23,6 +23,10 @@ static void InitializeFlipper(UIApplication *application) {
 }
 #endif
 
+@import UIKit;
+@import Firebase;
+
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -47,6 +51,9 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  [FIRApp configure];
+  
   return YES;
 }
 
